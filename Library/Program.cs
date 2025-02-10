53e164d3f1578c44dae9cd6a6401593c7a703194
halyna.felinska <g.felinskaya@gmail.com>
2025-02-10 18:24:22 +0200

    homeWork

--- Library/Program.cs
+++ Library/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main()
         {
-           Book forChildren = new Book("Lebiha", "Boys", 287); 
+           Book forChildren = new Book("Lebiha", "Boys") {PageSize = 289};
            forChildren.Price = 23;
            forChildren.Discount = 20;
 
-           Book forMen = new Book("Moroz", "realMen", 567);
+           Book forMen = new Book("Moroz", "realMen") {PageSize = 456};
            forMen.Price = 78;
            forMen.Discount = 37;
 
