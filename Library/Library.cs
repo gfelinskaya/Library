925dcef5fa1beaacbbb6a4b900c4a79a4c95c5da
Halyna Felinska <g.felinskaya@gmail.com>
2025-04-22 23:38:18 +0300

    homeWork

--- Library/Library.cs
+++ Library/Library.cs
@@ -8,6 +8,26 @@ namespace Lib
         {
             books = new Book[100];
         }
+
+        public Book this[int index] 
+        { 
+            get => books[index]; 
+        }
+        public Book this[string author]
+        {
+            get
+            {
+                for (int i = 0; i < books.Length; i++)
+                {
+                    if (books[i] != null && books[i].Author == author)
+                    {
+                        return books[i];
+                    }
+                }
+                return null;
+            }
+        }
+
         public void Add (Book book)
         {
             books[index] = book;
